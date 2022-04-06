6a5a2c4bf2895d1bcd06ea7ae489511023ac6771
alexKomocky <lalex1028@gmail.com>
2022-04-07 00:39:46 +0300

    heloo

---
+++ index.js
@@ -0,0 +1,20 @@
+let value = prompt ( 'vvidite chislo', 0 ) 
+if (value > 0) {
+    alert(1);
+} else if ( value < 0) {
+    alert (-1);
+} else {
+    alert ( 0 );
+}
+
+
+let result 
+
+if ( a + b < 4) ? 'malo' : ' mnogo';
+
+
+
+
+
+
+
